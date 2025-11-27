250e22e80825aabac1ff3853bed40b4c59798313
Tibi <tiberiutudorascu@gmail.com>
2025-11-27 21:43:16 +0200

    Finished CRC_CHECKSUM function

--- BLUESOLARTEST_ITCAN2/Core/Src/candispatch.c
+++ BLUESOLARTEST_ITCAN2/Core/Src/candispatch.c
@@ -2,13 +2,17 @@
 #include "stm32f1xx_hal.h"
 #include <stdbool.h>
 
-bool CRC_CHECKSUM(uint8_t crc_val_b)
-{
-uint8_t test = 2;
-if(crc_val_b == test)
-	return 1;
-else return 0;
+bool CRC_CHECKSUM(const CANMSG_T *CAN_MESSAGE) {
 
+	uint32_t crc_check_sum = CAN_MESSAGE->id + 8u + CAN_MESSAGE->data[0]
+			+ CAN_MESSAGE->data[1] + CAN_MESSAGE->data[2] + CAN_MESSAGE->data[3]
+			+ CAN_MESSAGE->data[4] + CAN_MESSAGE->data[5] + CAN_MESSAGE->data[6];
+
+	uint8_t expected_crc_check_sum = (uint8_t)(crc_check_sum & 0xFF);
+	if (expected_crc_check_sum == CAN_MESSAGE->data[7])
+		return 1;
+	else
+		return 0;
 }
 
 void BMS0x100(const CANMSG_T CAN_MESSAGE) {
@@ -24,23 +28,30 @@ void BMS0x100(const CANMSG_T CAN_MESSAGE) {
 	const uint8_t soh_raw = CAN_MESSAGE.data[5];        // octetul 6 = SOH
 	const uint8_t pack_amp_hours_raw = CAN_MESSAGE.data[6]; //octetul 7 = pack amphours
 	//IMPLEMENTARE CRC CHECKSUM
-	float BMSpackVoltage = v_raw * VOLT_CURR_SCALE; // [V]
-	float BMSpackCurrent = i_raw * VOLT_CURR_SCALE; // [A]
-	float BMSsoc = soc_raw * SOC_SCALE;        // [%]
-	float BMSsoh = soh_raw * SOH_TEMP_SCALE;   // [%]
-	float BMSamphours = pack_amp_hours_raw * VOLT_CURR_SCALE;
-
-	static char buf[96];
-	int n = snprintf(buf, sizeof(buf),
-	                 "BMS id=0x%03lX V=%.2fV I=%.2fA SOC=%.1f%% SOH=%.1f%% AMPH =%.1f%% \r\n",
-	                 (unsigned long)CAN_MESSAGE.id,
-	                 BMSpackVoltage, BMSpackCurrent, BMSsoc, BMSsoh,BMSamphours);
-	if (n < 0)
-		return;
-
-	if (HAL_UART_Transmit_IT(&huart, (uint8_t*) buf, (uint16_t) n) != HAL_OK) {
-
+	if (CRC_CHECKSUM(&CAN_MESSAGE) != 1) {
 		Error_Handler();
+	} else {
+
+		float BMSpackVoltage = v_raw * VOLT_CURR_SCALE; // [V]
+		float BMSpackCurrent = i_raw * VOLT_CURR_SCALE; // [A]
+		float BMSsoc = soc_raw * SOC_SCALE;        // [%]
+		float BMSsoh = soh_raw * SOH_TEMP_SCALE;   // [%]
+		float BMSamphours = pack_amp_hours_raw * VOLT_CURR_SCALE;
+
+		static char buf[96];
+		int n =
+				snprintf(buf, sizeof(buf),
+						"BMS id=0x%03lX V=%.2fV I=%.2fA SOC=%.1f%% SOH=%.1f%% AMPH =%.1f%% \r\n",
+						(unsigned long) CAN_MESSAGE.id, BMSpackVoltage,
+						BMSpackCurrent, BMSsoc, BMSsoh, BMSamphours);
+		if (n < 0)
+			return;
+
+		if (HAL_UART_Transmit_IT(&huart, (uint8_t*) buf, (uint16_t) n)
+				!= HAL_OK) {
+
+			Error_Handler();
+		}
 	}
 
 }
