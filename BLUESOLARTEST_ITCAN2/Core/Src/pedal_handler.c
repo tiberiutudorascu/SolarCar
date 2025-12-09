76e0cf8579b4385bd0756b2eeb1ed59d8762f09b
TamisiRaduNicolae <radu.tamisi@gmail.com>
2025-12-09 03:54:57 +0200

    added pedal_handler.c and pedal_handler.h files for developing throttle pedal logic
    implemented dual ADC polling ( ADC_Read ), signal filtering( Apply_Filter ), safety logic and error handling ( PedalStatus_t ENUM for clear error states, when shorted to VCC, wire broke and pin pulled down to GND, ect. ), safe state mechanism (Stop_Throttle())  and exposed Pedal_Init() and Pedal_Process() for interation in main loop

---
+++ BLUESOLARTEST_ITCAN2/Core/Src/pedal_handler.c
@@ -0,0 +1,189 @@
+/*
+ * pedal_handler.c
+ *
+ *  Created on: Dec 9, 2025
+ *      Author: tamis
+ */
+
+
+#include "stm32f1xx_hal.h"
+
+#include "pedal_handler.h"
+#include <stdlib.h> // pt functia de modul
+
+
+ extern ADC_HandleTypeDef hadc1; // referinta catre adc-ul din main.c definit acolo
+
+ PedalHandler_t gPedal; // --- Instanta globala a structurii pedalei ---
+
+
+ //COSNTANTE DE CALIBRARE , adc pe 12 biti => 0-4095 (0-3V3)
+
+#define ADC_MIN_VALID 100 // sub 0.08V inseamna ca e firul rupt  (pulldown ul intern trage la GND)
+
+#define ADC_MAX_VALID 4000	// peste 3.2V inseamna ca este scurtcircuit la 3.3V (VCC)
+
+#define MAX_DEVIATION 400   // eroare max acceptata intre senzori de 10%
+
+ 	 	 // --- buffer filtrare ---
+#define FILTER_SIZE 10
+ static uint16_t buf1[FILTER_SIZE] = {0};
+ static uint16_t buf2[FILTER_SIZE] = {0};
+ static uint8_t idx = 0;
+
+
+ //-------------------------------------------------------------------
+ //		-------___ FUNCTII PRIVATE ___-------
+
+ // --- filtru moving average (reduce zgomotul electric proportional cu FILTER_SIZE ) ---
+ static uint16_t Apply_Filter(uint16_t new_val, uint16_t *buffer){
+
+	 buffer[idx] = new_val;
+	 uint32_t sum =0;
+
+	 for(int i=0; i< FILTER_SIZE; i++){
+		 sum+= buffer[i];
+	 }
+
+	 return (uint16_t)(sum / FILTER_SIZE);
+ }
+
+
+ // --- citire canal ADC individual (polling ) ---
+ static uint16_t ADC_Read(uint32_t Channel){
+	 ADC_ChannelConfTypeDef sConfig = {0};
+	 uint16_t val =0 ;
+
+	 //reconfiguram ADCul pt canalul dorit
+	 sConfig.Channel = Channel;
+	 sConfig.Rank= ADC_REGULAR_RANK_1;
+	 sConfig.SamplingTime = ADC_SAMPLETIME_55CYCLES_5 ; // timp mai lung => mai stabil
+
+
+	 if(HAL_ADC_ConfigChannel(&hadc1, &sConfig )!= HAL_OK ){
+		 return 0 ;
+	 }
+
+
+	 // start -> asteapta -> citeste -> stop
+	 HAL_ADC_Start(&hadc1);
+	 if(HAL_ADC_PollForConversion(&hadc1, 10 ) == HAL_OK){
+		 val = HAL_ADC_GetValue(&hadc1);
+	 }
+
+	 HAL_ADC_Stop(&hadc1);
+
+
+	 return val;
+ }
+
+ //--- SAFE STATE (orpeste masina in caz de eroare ) ---
+
+ static void Stop_Throttle(void) {
+	 gPedal.throttle_pct =0; // facem pozitia acceleratiei 0 (taiem acceleratia)
+
+	 //aprindem Led-ul de eroare (pc13)
+	 HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+ }
+
+
+ //--------------------------------------------------------
+ //		-------___ FUNCTII PUBLICE ___-------
+
+
+ void Pedal_Init(void){
+	 gPedal.status = PEDAL_OK;
+	 gPedal.throttle_pct = 0;
+
+	 // stingem led-ul de eroare
+	 HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET); // SET este stins , iar RESET este aprins la blue pill
+ }
+
+
+ void Pedal_Process(void){
+
+	// citire raw pe PA0 si PA1
+	     gPedal.raw_adc1 = ADC_Read(ADC_CHANNEL_0);
+	     gPedal.raw_adc2 = ADC_Read(ADC_CHANNEL_1);
+
+	   // filtrare a semnalului
+	     gPedal.filt_adc1 = Apply_Filter(gPedal.raw_adc1, buf1);
+	     gPedal.filt_adc2 = Apply_Filter(gPedal.raw_adc2, buf2);
+
+
+
+	     idx = (idx + 1) % FILTER_SIZE;	// avansam indexul bufferului circular pentru urmatoarea tura
+
+
+//----------------- VERIFICARE  ERORI (safety checks )  ---------------------------------
+
+
+	  // check A: fir rupt (valoare prea mica )
+	     if (gPedal.filt_adc1 < ADC_MIN_VALID) {
+	         gPedal.status = ERR_POT1_DISCONNECTED;
+
+	         Stop_Throttle();
+
+	         return;
+	     }
+
+	     if (gPedal.filt_adc2 < ADC_MIN_VALID) {
+	         gPedal.status = ERR_POT2_DISCONNECTED;
+
+	         Stop_Throttle();
+
+	         return;
+	     }
+
+
+	   // check B: scurt la VCC ( valoare prea mare >3.2V)
+	     if (gPedal.filt_adc1 > ADC_MAX_VALID) {
+	         gPedal.status = ERR_POT1_SHORT;
+
+	         Stop_Throttle();
+
+	         return;
+	     }
+
+	     if (gPedal.filt_adc2 >	 ADC_MAX_VALID) {
+	   	         gPedal.status = ERR_POT2_SHORT;
+
+	   	         Stop_Throttle();
+
+	   	         return;
+	   	     }
+
+
+	 //    check C: plauzibilitatea citirii (  diferenta intre senzori  )
+
+	     // presup ca sunt senzori identici
+	     // daca diferenta e prea mare  =>  unul minte !
+
+	     if ( abs( (int)gPedal.filt_adc1 - (int)gPedal.filt_adc2 ) > MAX_DEVIATION ) {
+
+	         gPedal.status = ERR_IMPLAUSIBLE;
+
+	         Stop_Throttle();
+
+	         return;
+	     }
+
+
+	//  CALCUL FINAL ( totul OK )
+
+	     gPedal.status = PEDAL_OK;
+
+	     HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET); // safe state led OFF
+
+	     // mapare liniara simpla: 0-4095 -> 0-100%
+	     // folosim media celor 2 senzori pentru stabilitate maxima
+
+	     uint32_t avg = (gPedal.filt_adc1 + gPedal.filt_adc2) / 2;
+
+	     // limitam la  100% (sa nu depaseasca uint8 )
+	     if (avg > 4095) avg = 4095;
+
+	     gPedal.throttle_pct = (uint8_t)((avg * 100) / 4095); // exprimam in procente ( cat la suta este apasata acceleratia )
+
+ }
+
