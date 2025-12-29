b4e1b7712cc75876040935b9acf14c15b0d4a030
TamisiRaduNicolae <radu.tamisi@gmail.com>
2025-12-29 02:52:09 +0200

    Implemented Silixcon Motor simulations periodic TX messages
    -configured 20ms time triggered scheduler in main.c

--- MOTOR_C_SIM/Core/Src/main.c
+++ MOTOR_C_SIM/Core/Src/main.c
@@ -2,10 +2,11 @@
 /**
   ******************************************************************************
   * @file           : main.c
-  * @brief          : Main program body (Motor Controller SIM)
+  * @brief          : Main program body for MOTOR_C_SIM
   ******************************************************************************
   */
 /* USER CODE END Header */
+
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
@@ -13,7 +14,7 @@
 /* USER CODE BEGIN Includes */
 #include <stdbool.h>
 #include "pedal_handler.h"
-#include "motor_controller.h"
+#include "motor_controller.h" // aici sunt functiile de populare a datelor (Sim_X_X ...)
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -37,7 +38,7 @@ CAN_HandleTypeDef hcan;
 TIM_HandleTypeDef htim2;
 
 /* USER CODE BEGIN PV */
-volatile bool timer_20ms_flag = 0; // flag pentru ciclul de control
+volatile bool timer_20ms_flag = 0; // Flag setat de Timer la fiecare 20ms
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -53,7 +54,8 @@ static void MX_TIM2_Init(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
-// helper function pentru CAN TX
+/*  Helper Function: CAN_TX
+ */
 void CAN_TX(uint32_t StdId, uint32_t DLC, const uint8_t *Data) {
 	CAN_TxHeaderTypeDef txheader;
 	uint32_t txmailbox;
@@ -68,10 +70,12 @@ void CAN_TX(uint32_t StdId, uint32_t DLC, const uint8_t *Data) {
 	}
 }
 
-// Callback Timer
+/* * Callback Timer
+ * Se apeleaza automat la fiecare 20ms
+ */
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
 	if (htim->Instance == TIM2) {
-		timer_20ms_flag = 1; // facem flagul 1
+		timer_20ms_flag = 1; // Ridicam steagul
 	}
 }
 /* USER CODE END 0 */
@@ -89,17 +93,17 @@ int main(void)
   HAL_Init();
 
   /* Configure the system clock */
-  SystemClock_Config(); // Configurat pt 40MHz
+  SystemClock_Config(); // 40MHz pentru CAN 500kbps
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_ADC1_Init();
-  MX_CAN_Init(); // Init CAN
-  MX_TIM2_Init(); // Init Timer
+  MX_CAN_Init();
+  MX_TIM2_Init();
 
   /* USER CODE BEGIN 2 */
 
-  	  	  // Pornire CAN
+  // 1  Pornire CAN
   if (HAL_CAN_ActivateNotification(&hcan, CAN_IT_TX_MAILBOX_EMPTY | CAN_IT_BUSOFF) != HAL_OK) {
 	  Error_Handler();
   }
@@ -107,14 +111,24 @@ int main(void)
 	  Error_Handler();
   }
 
-  	  // Pornire Timer 20ms
+  // 2   Pornire Timer 20ms
   HAL_TIM_Base_Start_IT(&htim2);
 
-  	  	  // Pornire calibrare ADC
+  // 3   Pornire ADC si Calibrare
   HAL_ADCEx_Calibration_Start(&hadc1);
 
-  	  	  	  // Init logica pedala
+  // 4    Initializare Logica Pedala
   Pedal_Init();
+
+  /* Variabile pentru temporizare (Counters) */
+  uint8_t tick_100ms = 0;
+  uint8_t tick_250ms = 0; // de fapt este de 240ms, dar eroarea este neglijabila,
+  	  	  	  	  	  	  // pt ca e pt date afisate pe ecran unde nu e sesizabil (si pastram simplitatea avand doar TIM2)
+  uint8_t tick_800ms = 0;
+  uint8_t tick_1000ms = 0;
+
+  /* Buffer temporar pentru date */
+  uint8_t TxData[8];
 
   /* USER CODE END 2 */
 
@@ -126,20 +140,78 @@ int main(void)
 
     /* USER CODE BEGIN 3 */
 
-	  // (logica bazata pe flag )
+	  /* Asteptam steagul de la Timer (20ms) */
 	  if (timer_20ms_flag == 1) {
 
-	//  Procesam datele de la pedala
+		  // --- 1. Procesare Pedala (Citire intrari fizice) ---
 		  Pedal_Process();
 
-		// pregatim datele pentru Motor
-		  uint8_t motor_data[8];
-		  Motor_Prepare_Data(motor_data, &gPedal);
+		  // --- 2. Incrementare Contoare Timp ---
+		  tick_100ms++;
+		  tick_250ms++;
+		  tick_800ms++;
+		  tick_1000ms++;
 
-		  // trimitem pe CAN (  ID 0x5ff - Silixcon Control  )
-		  CAN_TX(0x5FF, 8, motor_data);
+		  // --- 3. Trimitere Mesaje Periodice (Simulare Motor) ---
 
-		  //  resetam flagul
+		  /* GRUP 100ms: Status, Motor Status, Battery */
+		  if (tick_100ms >= 5) { // 5 * 20ms = 100ms
+
+
+			  // A.   Status Message (0x600)
+			  Sim_Status_0x600(TxData); // Functia din motor_controller.c populeaza datele
+			  CAN_TX(0x600, 8, TxData);
+
+
+			  // B.  Motor Status (0x610) - RPM si Curent (Simulate din Pedala)
+			  Sim_MotorStatus_0x610(TxData, &gPedal);
+			  CAN_TX(0x610, 8, TxData);
+
+
+			  // C.  Battery Status (0x618)
+			  Sim_Battery_0x618(TxData);
+			  CAN_TX(0x618, 8, TxData);
+
+
+
+			  tick_100ms = 0; 	// reset counter
+
+			  // Blink LED la fiecare 100ms pentru vizualizare
+			  HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+
+		  }
+
+		  /* GRUP 250ms: Relative Values */
+		  if (tick_250ms >= 12) { // 12 * 20ms = 240ms (aprox 250)
+
+			  //  D.	 Relative Values (0x626)
+			  Sim_Relative_0x626(TxData, &gPedal);
+			  CAN_TX(0x626, 8, TxData);
+
+			  tick_250ms = 0;
+		  }
+
+		  /* GRUP 800ms: Temperature */
+		  if (tick_800ms >= 40) { // 40 * 20ms = 800ms
+
+			  //   E. 	Temperature Data (0x628)
+			  Sim_Temp_0x628(TxData);
+			  CAN_TX(0x628, 8, TxData);
+
+			  tick_800ms = 0;
+		  }
+
+		  /* GRUP 1000ms: Odometer */
+		  if (tick_1000ms >= 50) { // 50 * 20ms = 1s
+
+			  //   F. 	Odometer Data (0x620)
+			  Sim_Odometer_0x620(TxData);
+			  CAN_TX(0x620, 8, TxData);
+
+			  tick_1000ms = 0;
+		  }
+
+		  // Resetam flag-ul principal si asteptam urmatoarea intrerupere
 		  timer_20ms_flag = 0;
 	  }
   }
@@ -147,7 +219,7 @@ int main(void)
 }
 
 /**
-  * @brief System Clock Configuration (Setat pt 40MHz pt CAN stabil)
+  * @brief System Clock Configuration
   * @retval None
   */
 void SystemClock_Config(void)
@@ -156,23 +228,28 @@ void SystemClock_Config(void)
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
   RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
+  // Initializes the RCC Oscillators according to the specified parameters
+  // in the RCC_OscInitTypeDef structure.
+
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
-  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL10; // 40MHz
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL10; // 40MHz pentru CAN
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
   }
 
+ // Initializes the CPU, AHB and APB buses clocks
+
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2; // 20MHz
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1; // 40MHz
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
   {
@@ -187,10 +264,10 @@ void SystemClock_Config(void)
   }
 }
 
+
 static void MX_ADC1_Init(void)
 {
   ADC_ChannelConfTypeDef sConfig = {0};
-
   hadc1.Instance = ADC1;
   hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
   hadc1.Init.ContinuousConvMode = DISABLE;
@@ -198,25 +275,18 @@ static void MX_ADC1_Init(void)
   hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
   hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
   hadc1.Init.NbrOfConversion = 1;
-  if (HAL_ADC_Init(&hadc1) != HAL_OK)
-  {
-    Error_Handler();
-  }
+  if (HAL_ADC_Init(&hadc1) != HAL_OK) Error_Handler();
 
   sConfig.Channel = ADC_CHANNEL_0;
   sConfig.Rank = ADC_REGULAR_RANK_1;
   sConfig.SamplingTime = ADC_SAMPLETIME_55CYCLES_5;
-  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK) Error_Handler();
 }
 
-// config CAN
 static void MX_CAN_Init(void)
 {
   hcan.Instance = CAN1;
-  hcan.Init.Prescaler = 4; // pt 40MHz ---> 500kbps
+  hcan.Init.Prescaler = 4;
   hcan.Init.Mode = CAN_MODE_NORMAL;
   hcan.Init.SyncJumpWidth = CAN_SJW_1TQ;
   hcan.Init.TimeSeg1 = CAN_BS1_15TQ;
@@ -227,39 +297,28 @@ static void MX_CAN_Init(void)
   hcan.Init.AutoRetransmission = DISABLE;
   hcan.Init.ReceiveFifoLocked = DISABLE;
   hcan.Init.TransmitFifoPriority = DISABLE;
-  if (HAL_CAN_Init(&hcan) != HAL_OK)
-  {
-    Error_Handler();
-  }
+  if (HAL_CAN_Init(&hcan) != HAL_OK) Error_Handler();
 }
 
-// config TIM2 20ms
 static void MX_TIM2_Init(void)
 {
   TIM_ClockConfigTypeDef sClockSourceConfig = {0};
   TIM_MasterConfigTypeDef sMasterConfig = {0};
 
   htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 399; // 100kHz tick
+  htim2.Init.Prescaler = 399;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 1999;   // 20ms
+  htim2.Init.Period = 1999;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
-  {
-    Error_Handler();
-  }
+  if (HAL_TIM_Base_Init(&htim2) != HAL_OK) Error_Handler();
+
   sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
-  if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
+  if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK) Error_Handler();
+
   sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
   sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK) Error_Handler();
 }
 
 static void MX_GPIO_Init(void)
@@ -268,8 +327,8 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOC_CLK_ENABLE();
 
-  	  	  	  	  	  	  	  // led PC13 configurat pentru debug
   HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
+
   GPIO_InitStruct.Pin = GPIO_PIN_13;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
@@ -280,7 +339,5 @@ static void MX_GPIO_Init(void)
 void Error_Handler(void)
 {
   __disable_irq();
-  while (1)
-  {
-  }
+  while (1) {}
 }
--- MOTOR_C_SIM/Core/Src/motor_controller.c
+++ MOTOR_C_SIM/Core/Src/motor_controller.c
@@ -1,47 +1,122 @@
 /*
  * motor_controller.c
- *
- *  Created on: Dec 22, 2025
- *      Author: tamis
  */
-
 
 #include "motor_controller.h"
 
+#define RPM_CONSTANT  50
+#define CURRENT_CONSTANT  3
+#define GEAR_RATIO  50 // presupunem ca    RPM/real_speed = 50
+#define MC_VOLTAGE  50 // presupunem teniunea de la motor controller 50V
 
-
-void Motor_Prepare_Data(uint8_t *buffer, PedalHandler_t *pedal) {
-
-
-    // Resetam bufferul
+/* Helper: Curata bufferul inainte de scriere */
+static void Clear_Buffer(uint8_t *buffer) {
     for(int i=0; i<8; i++) buffer[i] = 0;
+}
 
-    int16_t target_torque = 0;
+/* --- 1    Status Messages (0x600) --- */
+void Sim_Status_0x600(uint8_t *buffer) {
+    Clear_Buffer(buffer);
 
-    // logica de cuplu
-    if (pedal->status == PEDAL_OK && !pedal->safe_state_locked){
-        // Mapare: 100% = 1000 unitati cuplu
-        target_torque = (pedal->throttle_pct * 10);
+    // Byte 0: LYNX ID (Arbitrar 10)
+    buffer[0] = 10;
+
+    // Byte 1: LYNX Mode ( CRITIC!)
+    // 0x03  = Operational (running) ; daca trimitem 0, VCU va crede ca motorul nu functioneaza
+    buffer[1] = 0x03;
+
+    // Byte 4-5: Driver Limit (0xFFFF = Fara limita)
+    buffer[4] = 0xFF;
+    buffer[5] = 0xFF;
+}
+
+/* --- 2    Motor Status (0x610) --- */
+void Sim_MotorStatus_0x610(uint8_t *buffer, PedalHandler_t *pedal) {
+    Clear_Buffer(buffer);
+
+
+
+    //     SIMULARE FIZICA :
+    // daca pedala e apasata, crestem RPM-ul si curentul simulat
+
+
+
+    int16_t sim_rpm = 0;
+    int16_t sim_current = 0;
+
+    if (pedal->status == PEDAL_OK && !pedal->safe_state_locked) {
+        sim_rpm = pedal->throttle_pct * RPM_CONSTANT;    // 100% = 5000 RPM
+        sim_current = pedal->throttle_pct * CURRENT_CONSTANT; // 100% = 300 Amperi
     }
 
-    else{
-        target_torque = 0;
-    }
+    // Byte 0-1: Motor Current [A]
+    buffer[0] = sim_current & 0xFF;
+    buffer[1] = (sim_current >> 8) & 0xFF;
 
-    // Byte 0-1: Torque (Little Endian)
-    buffer[0] = target_torque & 0xFF;
-    buffer[1] = (target_torque >> 8) & 0xFF;
 
-    // Byte 2-3: Target Speed (Optional, punem 0 sau Max)
-    buffer[2] = 0x00;
-    buffer[3] = 0x00;
+    // Byte 2-3: Motor Speed [RPM]
+    buffer[2] = sim_rpm & 0xFF;
+    buffer[3] = (sim_rpm >> 8) & 0xFF;
 
-    // Byte 4: Control Flags (Bit 0 = Enable Inverter)
-    if ( pedal->status == PEDAL_OK  && !pedal->safe_state_locked  ){
-        buffer[4] = 0x01; // Enable
-    } else {
-        buffer[4] = 0x00; // Disable / Safe Mode
-    }
 
-    // Bytes 5,6,7 raman 0
+    // Byte 4-5: Vehicle Speed [km/h] (Estimare bruta: RPM / 50)
+    int16_t sim_kph = sim_rpm / GEAR_RATIO;
+
+    buffer[4] = sim_kph & 0xFF;
+    buffer[5] = (sim_kph >> 8) & 0xFF;
+
+
+    // Byte 6-7: Power [ W ] (Estimare: Current * 50V)
+    int16_t sim_power = sim_current * MC_VOLTAGE;
+
+    buffer[6] = sim_power & 0xFF;
+    buffer[7] = (sim_power >> 8) & 0xFF;
+}
+
+/* --- 3. Battery Status (0x618) --- */
+void Sim_Battery_0x618(uint8_t *buffer) {
+    Clear_Buffer(buffer);
+
+    // Byte 0: Status Bits (Bit 2 = BMS Connected)
+    buffer[0] = 0x04;
+
+    // Byte 2: SOC [%]
+    buffer[2] = 88; // random value (baterie incarcata la 88%)
+
+    // Byte 4-5: Battery Voltage [0.01 V]
+    // 48V = 4800 unitati
+    uint16_t volts = 4800;
+    buffer[4] = volts & 0xFF;
+    buffer[5] = (volts >> 8) & 0xFF;
+}
+
+/* --- 4. Odometer Data (0x620) --- */
+void Sim_Odometer_0x620(uint8_t *buffer) {
+    Clear_Buffer(buffer);
+
+    // Byte 0-3: TRIP (UINT32) - Lasam fix momentan
+    buffer[0] = 0x50; // 0x50 = 80 unitati (0.8km)
+}
+
+/* --- 5. Relative Values (0x626) -----   */
+void Sim_Relative_0x626(uint8_t *buffer, PedalHandler_t *pedal) {
+	Clear_Buffer(buffer);
+
+	//  	simulam puterea relativa  pentru  Display 	 (-32767 ... +32767)
+	int16_t rel_val = pedal->throttle_pct * 300;
+	buffer[0] = rel_val & 0xFF;
+	buffer[1] = (rel_val >> 8) & 0xFF;
+}
+
+/* --- 6. Temperature Data (0x628) --- */
+void Sim_Temp_0x628(uint8_t *buffer) {
+    Clear_Buffer(buffer);
+
+    // Byte 0-1: Motor Temp [0.1 C] -> 50.0 C = 500
+    uint16_t m_temp = 500;
+    buffer[0] = m_temp & 0xFF;
+    buffer[1] = (m_temp >> 8) & 0xFF;
+
+    // Byte 4: Driver Temp [1 C] -> 35 C
+    buffer[4] = 35;
 }
